d79817e7887c30f72aeee7d7b633e83cfa29123b
Alex Siegberg and Nathan Wangemann <nathan@>
2019-06-12 14:37:52 -0700

    remove unused code lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 const vowels = ["a", "e", "i", "o", "u"];
-//const consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
 function isAVowel(letter) {
   for(var vowel = 0; vowel < vowels.length; vowel++) {
@@ -19,31 +18,20 @@ function pigLatin(sentence) {
 
 function translatePigLatinWord(word) {
   word = word.split("");
-
-  //for(var vowel = 0; vowel < vowels.length; vowel++) {
     if(isAVowel(word[0]) && word.length === 1) {
       return word.join("") + "ay";
     }
-  //}
-
-  //for(var consonant = 0; consonant < consonants.length; consonant++) {
     if (! isAVowel(word[0])) {
       var consonantSlice = word.splice(0, 1);
-      //consonantSlice =
-
       word.push(consonantSlice.toString().toUpperCase());
       return word.join("") + "ay";
     }
-  //}
 
   for(var character = 0; character < word.length; character++) {
-    //for(var vowel = 0; vowel < vowels.length; vowel++) {
       if (word.length > 1 && isAVowel(word[character])) {
         return word.join("") + "Way";
       }
-    //}
   }
-
   return word.join("");
 };
 
